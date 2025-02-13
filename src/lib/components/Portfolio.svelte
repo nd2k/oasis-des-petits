7f67a2cfb5b1a457e145dde9083f56bb8e5c9e4d
nd2k <nicolas.decat@gmail.com>
2025-02-14 00:11:21 +0100

    fix porfolio

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -117,72 +117,77 @@
 {/if}
 
 <style>
-    .grid {
+    .portfolio {
         position: relative;
-        column-count: 2;
-        gap: var(--size-px-2);
-        & .grid-item {
-            margin: 0;
-            margin-bottom: var(--size-px-1);
-            & .btn {
+        padding: var(--size-fluid-2) 0;
+        .grid {
+            position: relative;
+            column-count: 2;
+            gap: var(--size-px-3);
+            & .grid-item {
                 margin: 0;
-                padding: 0;
-                border: none;
-                & .thumbnail {
-                    width: 200px;
-                    height: auto;
+                margin-bottom: var(--size-px-1);
+                & .btn {
+                    margin: 0;
+                    padding: 0;
+                    border: none;
+                    & .thumbnail {
+                        width: 190px;
+                        height: auto;
+                    }
                 }
             }
         }
     }
+
     .lightbox {
-        display: flex;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5);
-        justify-content: center;
-        align-items: center;
-        z-index: 1000;
-        & img {
-            max-width: 90%;
-            max-height: 90%;
-            border-radius:  var(--radius-3);
-            animation: fadeIn 2s;
+            display: flex;
+            position: fixed;
+            top: 0;
+            width: 100vw;
+            height: 100vh;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+            z-index: 1000;
+            & img {
+                max-width: 90%;
+                max-height: 90%;
+                border-radius:  var(--radius-3);
+                animation: fadeIn 2s;
+            }
+            & .prev,
+            & .next {
+                height: 3rem;
+                width: 3rem;
+                position: absolute;
+                transform: translateY(-50%);
+                font-size: var(--size-fluid-4);
+                background-color: transparent;
+                color: hsl(var(--white-0));
+                padding: var(--size-px-1);
+                cursor: pointer;
+                z-index: 1001;
+                border: none;
+            }
+            & .prev {
+                left: var(--size-fluid-2);
+            }
+            & .next {
+                right: var(--size-fluid-2);
+            }
+            & .close {
+                position: absolute;
+                top: var(--size-fluid-4);
+                right: var(--size-fluid-3);
+                font-size: var(--size-fluid-4);
+                color: hsl(var(--white-0));
+                background: none;
+                cursor: pointer;
+                border: none;
+                z-index: 1001;
+            }
         }
-        & .prev,
-        & .next {
-            height: 3rem;
-            width: 3rem;
-            position: absolute;
-            transform: translateY(-50%);
-            font-size: var(--size-fluid-4);
-            background-color: transparent;
-            color: hsl(var(--white-0));
-            padding: var(--size-px-1);
-            cursor: pointer;
-            z-index: 1001;
-            border: none;
-        }
-        & .prev {
-            left: var(--size-fluid-2);
-        }
-        & .next {
-            right: var(--size-fluid-2);
-        }
-        & .close {
-            position: absolute;
-            top: var(--size-fluid-3);
-            right: var(--size-fluid-3);
-            font-size: var(--size-fluid-4);
-            color: hsl(var(--white-0));
-            background: none;
-            cursor: pointer;
-            border: none;
-            z-index: 1001;
-        }
-    }
 
     @keyframes fadeIn {
         from {
